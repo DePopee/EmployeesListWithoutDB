7ac2290bda908646910f25595df4e63b1ab91404
DePopee <immanuel.solomonj@gmail.com>
2024-02-21 16:15:02 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeService {
+    private static Map<Long, Employee> employees= new HashMap<>();
+    private static Long index=2L;
+
+    static {
+        Employee employee1 = new Employee(1L, "emp1", "9999999999", 200000L);
+        Employee employee2 = new Employee(2L, "emp2", "8888888888", 300000L);
+        employees.put(1L, employee1);
+        employees.put(2L, employee2);
+    }
+
+    public static List<Employee> getAllEmployees() {
+        return new ArrayList<>(employees.values());
+    }
+
+    public static Employee getEmployeesDetails(Long employeeId){
+        return employees.get(employeeId);
+    }
+
+    public static Employee addEmployee(Employee employee) {
+        index += 1;
+        employee.setId(index);
+        return employee;
+    }
+
+    public static Employee updateEmployee(Long employeeId,  Employee employee) {
+        employee.setId(employeeId);
+        employees.put(employeeId, employee);
+        return employee;
+    }
+
+    public static Employee deleteEmployee(Long employeeId) {
+        return employees.remove(employeeId);
+    }
+
+
+}
